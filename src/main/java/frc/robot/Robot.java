6876639ba503fea054774647b66aa5a628500328
sophia-sangui <sophia.sangiuliano@rogers.com>
2025-01-27 18:48:33 -0500

    added limelight

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.LimelightHelpers.LimelightTarget_Fiducial;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -80,7 +83,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    SmartDashboard.putString("Limelight", LimelightHelpers.getLatestResults("Left").toString());
+  }
 
   @Override
   public void testInit() {
